d14a4020de0c4990ecb50459c853f51304bc2b75
ArmandXiao <920338028@qq.com>
2020-03-05 19:15:56 +0800

    获取英文单词的发音，词性，和意思。

---
+++ PyQt5_GUI/getTranslationFromYouDao.py
@@ -0,0 +1,38 @@
+from urllib import parse, request
+import re
+
+
+def translate(word):
+    '''
+    Can translate from English to Chinese Only
+    :param word: an English word
+    :return: two lists. One contains pronounciations of the word. The other gives all meanings gotten from the website.
+    '''
+    word = word.replace(" ", "%20")
+    url = "http://dict.youdao.com/w/" + word + "/#keyfrom=dict2.top"
+    res = request.urlopen(url)
+
+    html = res.read().decode("utf-8")
+    pronounce = re.compile(r"<span class=\"phonetic\">.+?</span>")
+    meaning = re.compile(r"<li>[^<>].*?</li>")
+
+    proListraw = pronounce.findall(html)
+    meanListraw = meaning.findall(html)
+
+    proList = []
+    meanList = []
+    # 第一个是英式发音 第二个是美式发音
+    for each in proListraw:
+        proList.append(re.search(r"[[].+[]]", each).group())
+
+    for each in meanListraw:
+        tmp = re.search(r">.+<", each).group()
+        meanList.append(tmp[1:len(tmp) - 1])
+
+    return proList, meanList
+
+
+if __name__ == '__main__':
+    lista, listb = translate("Fluoridation")
+    print(lista)
+    print(listb)
